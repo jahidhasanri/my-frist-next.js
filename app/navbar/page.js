4f274b047910a01e273a4565a40bcf35812e5163
jahid hasan rifat <jahadhasanrifat@gmail.com>
2025-01-23 20:44:20 +0600

    add domain url

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -27,7 +27,7 @@ export default async function Navbar() {
         <div>
           {user ? (
             <div className="flex items-center space-x-4">
-              {/* <span className="text-white">Welcome, {user.given_name || user.email}</span> */}
+              <span className="text-white">Welcome, {user.given_name || user.email}</span>
               <LogoutLink>
                 <button className="bg-red-500 px-4 py-2 rounded hover:bg-red-700">
                   Logout
