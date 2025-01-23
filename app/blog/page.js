275a05884893a84cede4e39765292df6ee0e3bf4
jahid hasan rifat <jahadhasanrifat@gmail.com>
2025-01-24 00:46:54 +0600

    Wrap button inside an anchor tag

--- app/blog/page.js
+++ app/blog/page.js
@@ -16,12 +16,14 @@ export default async function blog() {
           >
               <span>{post.title}</span>
               <Link href={`/blog/${post.id}`} className="text-blue-500 text-lg font-semibold">
+                <a>
                 <button 
                   className="btn bg-green-300 text-gray-600 mt-2 px-4 py-2 rounded"
                   aria-label={`Read more about ${post.title}`}
                 >
                   Click to read more...
                 </button>
+                </a>
               </Link>
           </li>
         ))}
